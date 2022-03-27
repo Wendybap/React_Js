776ba062b4e63243dea940598dd36404abf8d122
wendy Arcia <wendy_342@hotmail.com>
2022-03-27 12:10:55 -0300

    Desafío 2: Creación del Navbar

---
+++ app-wendy-arcia/src/componentes/NavBar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Ecommerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <NavDropdown title="Categorias" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action2">Moda</NavDropdown.Item>
+                <NavDropdown.Item href="#action3">
+                  Belleza y cuidado personal
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action4">
+                  Decoraciones
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#action5">Crea tu cuenta</Nav.Link>
+              <Nav.Link href="#action6">Iniciar Sesión</Nav.Link>
+              <Nav.Link href="#action6" disabled>
+                Carrito
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Buscar"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Buscar</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
